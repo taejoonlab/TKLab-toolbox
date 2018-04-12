00d6efbc00577205fb768fedabf52f532c534a4a
Taejoon Kwon <tkwon@unist.ac.kr>
2018-04-12 09:46:54 +0900

    A script to convert fast5 to fastq

---
+++ fastq/fast5-to-fastq.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+## Install h5py via 'conda install h5py'
+import h5py
+
+## Script to convert Oxfort NanoPore fast5 to fastq
+## Ref: https://gist.github.com/nickloman/3345d05e3a4642b5306c
+
+fq_types = {'template' : '/Analyses/Basecall_2D_000/BaseCalled_template/Fastq',
+            'complement' : '/Analyses/Basecall_2D_000/BaseCalled_complement/Fastq',
+            'twodirections' : '/Analyses/Basecall_2D_000/BaseCalled_2D/Fastq'}
+
+filename_f5 = sys.argv[1]
+
+hdf5 = h5py.File(filename_f5,'r')
+for tmp_type, tmp_path in fq_types.iteritems():
+    try:
+        fq_lines = hdf5[tmp_path][()].strip().split("\n")
+        fq_lines[0] = "%s %s"%(fq_lines[0],tmp_type)
+        print( "\n".join(fq_lines) )
+    except Exception, e:
+        sys.stderr.write('Error:',e,'\n')
+        pass
+hdf5.close()
