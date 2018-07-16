4da943f114e3e5cf64021660a59bbc20354e8134
Taejoon Kwon <tkwon@unist.ac.kr>
2018-07-16 09:12:13 +0900

    Revise fetch-seq-by-header.py with flake8

--- fasta/fetch-seq-by-header.py
+++ fasta/fetch-seq-by-header.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import os
 import sys
 
 filename_fa = sys.argv[1]
@@ -7,18 +6,18 @@ filename_list = sys.argv[2]
 
 seq_h = ''
 seq_list = dict()
-f_fa = open(filename_fa,'r')
+f_fa = open(filename_fa, 'r')
 for line in f_fa:
-    if( line.startswith('>') ):
+    if line.startswith('>'):
         seq_h = line.strip().lstrip('>')
         seq_list[seq_h] = []
     else:
-        seq_list[seq_h].append( line.strip() )
+        seq_list[seq_h].append(line.strip())
 f_fa.close()
 
-f_list = open(filename_list,'r')
+f_list = open(filename_list, 'r')
 for line in f_list:
     tokens = line.strip().split()
-    if( seq_list.has_key(tokens[0]) ):
-        print ">%s\n%s"%(tokens[0], ''.join(seq_list[tokens[0]]))
+    if tokens[0] in seq_list:
+        print(">%s\n%s" % (tokens[0], ''.join(seq_list[tokens[0]])))
 f_list.close()
