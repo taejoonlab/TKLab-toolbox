0634b460f55aa80a0f9f30693fe39d2c4e61e2d3
Taejoon Kwon <tkwon@unist.ac.kr>
2018-08-25 11:25:01 +0900

    A bug fix for run-trimmomatic

--- fastq/run-trimmomatic.sh
+++ fastq/run-trimmomatic.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 ## Assume that you install trimmomatic with conda
-
 ## Directory for conda
 DIR_CONDA=$HOME/miniconda3
 
-FA_ADAPTER=$( find $DIR_CONDA | grep -m1 TruSeq3-PE-2.fa )
+FA_ADAPTER=$( find $DIR_CONDA | grep -m1 TruSeq3-PE.fa )
 echo "Adapter: "$FA_ADAPTER
+cp $FA_ADAPTER .
 
 for FQ1 in $(ls *R1.*fastq)
 do
   FQ2=${FQ1/_R1/_R2}
   OUT=${FQ1/_R1.raw.fastq}"_trim"
+  echo $FQ1 $FQ2 $OUT
 
   trimmomatic PE -validatePairs -summary $OUT".summary" $FQ1 $FQ2 -baseout $OUT \
-    $FA_ADAPTER:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
+   ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
 done
 
