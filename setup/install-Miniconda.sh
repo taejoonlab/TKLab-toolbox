7a773197a72998ffe24182356ef186d009b3af17
Taejoon Kwon <tkwon@unist.ac.kr>
2019-07-26 20:30:04 +0900

    Add a comment on Miniconda

--- setup/install-Miniconda.sh
+++ setup/install-Miniconda.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# If 'conda' is available, skip this.
 if [ -x "$(command -v conda)" ]; then
   echo "conda is avilalbe. Skip installation."
 else
-  echo "Install the latest Miniconda3"
+
+  echo "Download the latest Miniconda3"
   MINICONDA_INSTALLER="Miniconda3-latest-Linux-x86_64.sh"
   wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER
+  
+  echo "Install Miniconda3"
   chmod 755 $MINICONDA_INSTALLER
   ./$MINICONDA_INSTALLER
   rm $MINICONDA_INSTALLER
