54eaeca987ff66d4eecf94742c0c833ee7cd3e39
Taejoon Kwon <tkwon@unist.ac.kr>
2019-12-26 18:05:04 +0900

    Add 'apt-get update' before installing cuda

--- setup/install-cuda-xenial.sh
+++ setup/install-cuda-xenial.sh
@@ -6,4 +6,5 @@ curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_6
 sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 
+sudo apt-get update
 sudo apt-get install cuda
