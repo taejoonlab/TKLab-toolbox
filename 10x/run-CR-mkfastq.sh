017531367430d256bfe343b5c42b791d0b9f973a
Taejoon Kwon <taejoon.kwon@gmail.com>
2020-03-15 00:37:10 +0900

    Add a comment to search for 10x barcode index

--- 10x/run-CR-mkfastq.sh
+++ 10x/run-CR-mkfastq.sh
@@ -16,5 +16,13 @@
 ID="scCapSt27"
 INPUT_DIR="./181121_NB501779_0090_AHWYLVBGX7"
 
+# If you don't know the barcode index, run 'cellranger mkfastq' with arbitrary barcode index first.
+# Then, look at the any of following files under <sampleID>/outs/fastq_path/Stats directory.
+#  - ConversionStats.xml
+#  - DemuxSummaryFIL?.txt
+#  - Stats.json
+# You can find the most abundant "undetermined" barcode sequence. 
+# Search the 10x/*csv file with that undetermiend barcode sequence, 
+# then you will see what your barcode index is.
 
 ~/src/10X/cellranger-2.1.1/cellranger mkfastq --id=$ID --run=$INPUT_DIR --csv="samplesheet-"$ID".csv"
