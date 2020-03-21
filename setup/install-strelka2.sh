458d8712e4352c9c146a92ccd51db226a2760bff
Taejoon Kwon <taejoon.kwon@gmail.com>
2020-03-21 18:03:05 +0900

    revise the script to install strelka2

--- setup/install-strelka2.sh
+++ setup/install-strelka2.sh
@@ -3,4 +3,10 @@
 # https://github.com/Illumina/strelka/blob/v2.9.x/docs/userGuide/quickStart.md
 # https://github.com/Illumina/strelka/releases
 
-wget wget https://github.com/Illumina/strelka/releases/download/v2.9.2/strelka-2.9.2.centos6_x86_64.tar.bz2
+# From the github
+# wget https://github.com/Illumina/strelka/releases/download/v2.9.2/strelka-2.9.2.centos6_x86_64.tar.bz2
+
+# From bioconda. It requires python2.7
+conda create py2 python=2.7
+conda install -c bioconda strelka
+
