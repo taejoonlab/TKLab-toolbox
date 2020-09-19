3e31ab6498d857897e95c8fe923cadb6b3aadc5a
Taejoon Kwon <tkwon@unist.ac.kr>
2020-09-19 23:05:36 +0900

    minor bug fix

--- homer/03-findMotifGenome.sh
+++ homer/03-findMotifGenome.sh
@@ -3,14 +3,15 @@
 ## FASTA of genome sequences
 #GENOME="MyGenome.fa"
 
-TAGS_T="tags.MyTF"
-MOTIFS_T=${TAGS_T/tags./motifs}
+#TAGS_T="tags.MyTF"
+
+MOTIFS_T=${TAGS_T/tags./motifs.}
 PEAKS=$TAGS_T"/peaks.txt"
 
 if [ ! -e $MOTIFS_T ]; then
   mkdir $MOTIFS_T
 fi
 
-POS=$TAGS_T".motif_pos.txt"
+POS=$MOTIFS_T".motif_pos.txt"
 cp $PEAKS $MOTIFS_T
-findMotifsGenome.pl $PEAKS $GENOME $MOTIF_DIR  > $POS
+findMotifsGenome.pl $PEAKS $GENOME $MOTIFS_T > $POS
