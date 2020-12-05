26011e215229a07d7aa7b9c8aced7676ebcb2eb8
Taejoon Kwon <tkwon@unist.ac.kr>
2020-12-05 19:38:36 +0900

    add a command for GFF3 and gmap_build

--- align/run-gmap.sh
+++ align/run-gmap.sh
@@ -3,11 +3,17 @@
 # Install GMAP using conda
 # $ conda install -c bioconda gmap
 
+# Make GMAP database with gmap_build
+# $ gmap_build -d $DBNAME -D $GMAPDB $DB_FASTA
+
 GMAPDB="path/to/db.gmap/"
 NUM_THREADS=1
 
 FA="<my fasta file>"
-DB="<db index name>""
+DBNAME="<db index name>"
 
 OUT=${FA/.fa/}"."$DB".gmap_psl"
 time gmap -f psl -t $NUM_THREADS -D $GMAPDB -d $DB $FA > $OUT
+
+# for GFF output
+# time gmap -f gff3_gene -t $NUM_THREADS -D $GMAPDB -d $DB $FA > $OUT
