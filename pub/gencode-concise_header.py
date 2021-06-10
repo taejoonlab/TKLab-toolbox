38e8d23492107d55fbb3e3feb8c7f7df9130ca29
Taejoon Kwon <taejoon.kwon@gmail.com>
2021-06-11 00:08:48 +0900

    add species info on concise header

--- pub/gencode-concise_header.py
+++ pub/gencode-concise_header.py
@@ -11,7 +11,15 @@ if filename_fa.endswith('.gz'):
 for line in f_fa:
     if line.startswith('>'):
         tmp_tokens = line.strip().lstrip('>').split('|')
-        print(">%s|%s|%s" % (tmp_tokens[-2], tmp_tokens[0], tmp_tokens[2]))
+        tmp_g_id = tmp_tokens[0]
+        tmp_species = 'Unknown'
+        if tmp_g_id.startswith('ENSG0'):
+            tmp_species = 'HUMAN'
+        if tmp_g_id.startswith('ENSMUSG0'):
+            tmp_species = 'MOUSE'
+
+        print(">%s|%s|%s|%s" %
+              (tmp_tokens[-2], tmp_species, tmp_tokens[0], tmp_tokens[2]))
     else:
         print(line.strip())
 f_fa.close()
