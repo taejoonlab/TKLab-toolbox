420b4043304444ab0bc03e2d4027240f4d65cfd7
Taejoon Kwon <taejoon.kwon@gmail.com>
2021-10-03 12:34:11 +0900

    split low abundance tx from kallisto table

--- express/kallisto-to-tpm_tbl.py
+++ express/kallisto-to-tpm_tbl.py
@@ -30,14 +30,24 @@ for line in f_conf:
 f_conf.close()
 
 tx_list = sorted(list(set(tx_list)))
-f_out = open('%s.kallisto.txt' % (filename_conf.replace('.conf', '')), 'w')
+f_out = open('%s.kallisto_tpm.txt' % (filename_conf.replace('.conf', '')), 'w')
+f_low = open('%s.kallisto_tpm.low.log' % (filename_conf.replace('.conf', '')), 'w')
 f_out.write('SeqID\t%s\n' % ('\t'.join(sample_list)))
+f_low.write('SeqID\t%s\n' % ('\t'.join(sample_list)))
 for tmp_tx in tx_list:
     out_str = []
+    out_tpm_list = []
     for tmp_sample in sample_list:
         if tmp_sample in tpm_list and tmp_tx in tpm_list[tmp_sample]:
             out_str.append('%.3f' % tpm_list[tmp_sample][tmp_tx])
+            out_tpm_list.append(tpm_list[tmp_sample][tmp_tx])
         else:
             out_str.append('%.3f' % (0.0))
-    f_out.write('%s\t%s\n' % (tmp_tx, '\t'.join(out_str)))
+    
+    sum_tpm = sum(out_tpm_list)
+    if sum_tpm > 1.0 and out_tpm_list.count(0.0) < (len(out_tpm_list)-2):
+        f_out.write('%s\t%s\n' % (tmp_tx, '\t'.join(out_str)))
+    else:
+        f_low.write('%s\t%s\n' % (tmp_tx, '\t'.join(out_str)))
 f_out.close()
+f_low.close()
