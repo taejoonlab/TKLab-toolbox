6d71c9e2e3089ba3e9139997d2c5ac6ec29eaa73
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-05-22 12:15:27 +0900

    relocate the script to run FLASH

---
+++ ngs.pre/run-flash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NUM_THREADS=4
+MAX_OVERLAP=140  # ReadLength - 10 bp
+
+#for R1 in $(ls *_R1.raw.fastq.gz)
+for R1 in $(ls *_R1.trim.fastq.gz)
+do
+  R2=${R1/_R1/_R2}
+
+  OUT_NAME=${R1/_R1/}
+  #OUT_NAME=${R1/.raw.fastq.gz/}
+  OUT_NAME=${R1/.trim.fastq.gz/}
+
+  echo $R1 $R2 $OUT_NAME
+
+  flash $R1 $R2 -o $OUT_NAME -t $NUM_THREADS -M $MAX_OVERLAP --interleaved-output
+done
+
